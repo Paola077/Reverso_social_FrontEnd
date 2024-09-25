e2dcb3f31c6dda6da2e8df0b6e48c34e9a206665
indiakka <78439130+indiakka@user.noreply.github.com>
2024-09-25 14:47:49 +0200

    put gradient in reverso

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,21 +1,20 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "../components/footer/Footer";
 import Navbar from "../components/navbar/Navbar";
+import "./_Layout.scss"; 
 
-const Layout = () =>
-{
+const Layout = () => {
   return (
-    <div>
-      <Navbar/>
-      <main>
-        <Outlet />
-        
-      </main>
-      <Footer />
+    <div className="layout-container">
+      <div className="gradient-reverso">
+        <Navbar className="layout-navbar" />
+        <main>
+          <Outlet />
+        </main>
+      </div>
+      {/* <Footer className="layout-footer" /> */}
     </div>
-    
   );
-}
-
+};
 
 export default Layout;
