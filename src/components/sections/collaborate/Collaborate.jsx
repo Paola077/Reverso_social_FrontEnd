add5dc340ac243e36b27e1504107ef98f080e575
Laura Pino <lpigueroa@gmail.com>
2024-09-25 09:31:32 +0200

    Upload changes for commit

---
+++ src/components/sections/collaborate/Collaborate.jsx
@@ -0,0 +1,15 @@
+import "./_Collaborate.scss";
+
+const Collaborate = () => {
+  return (
+    <>
+        <div className="container">
+            <div className="container__contact">
+              <h3 className="container__contact__title">¿Nos ayudas?</h3>
+            </div>
+            <div className="container__image"></div>
+        </div>
+    </>
+  )
+};
+export default Collaborate;
--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Collaborate from "../components/sections/collaborate/Collaborate";
 
 const Layout = () => {
   return (
     <main>
       <Outlet />
+      <Collaborate />
     </main>
   );
 };
