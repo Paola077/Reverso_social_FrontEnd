ec5e433c2139c62abe8a66cf8bec35fc0609ff66
MabelZar <mabel.zar.esp@gmail.com>
2024-09-26 14:56:35 +0200

    login form styles

--- src/components/forms/logInForm/SignInUpForm.jsx
+++ src/components/forms/logInForm/SignInUpForm.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import './_SignInUpForm.scss'
+import React, { useState } from "react";
+import "./_SignInUpForm.scss";
+import logoFemsenior from "../../../../public/images/logoFemsenior.svg";
+import logoReversoSocial from "../../../../public/images/logoReversoSocial.svg";
 
-const SignInUpForm=({ defaultToSignUp = false })=>{
-
-	const [isRightPanelActive, setIsRightPanelActive] = useState(defaultToSignUp);
+const SignInUpForm = ({ defaultToSignUp = false }) => {
+  const [isRightPanelActive, setIsRightPanelActive] = useState(defaultToSignUp);
 
   const handleSignUpClick = () => {
     setIsRightPanelActive(true);
@@ -12,48 +13,61 @@ const SignInUpForm=({ defaultToSignUp = false })=>{
   const handleSignInClick = () => {
     setIsRightPanelActive(false);
   };
-    return(
-        <>
-   
-<div className={`container ${isRightPanelActive ? 'right-panel-active' : ''}`} id="container">
-	<div className="form-container sign-up-container">
-		<form action="#">
-			<h2>Crea una cuenta</h2>
-			<input type="text" placeholder="Nombre" />
-			<input type="email" placeholder="Email" />
-			<input type="date" placeholder="Fecha de nacimiento" />
-			<input type="password" placeholder="Contraseña" />
-			<button>Entrar</button>
-		</form>
-	</div>
-	<div className="form-container sign-in-container">
-		<form action="#">
-			<h2>Accede a tu cuenta</h2>
-			<input type="email" placeholder="Email" />
-			<input type="password" placeholder="Contraseña" />
-			<button>Entrar</button>
-			<p href="#">No tienes cuenta? Accede 
-				{" "}
-			<span className="ghost" onClick={handleSignUpClick} id="signUp"> Aqui</span>
-			</p>
-		</form>
-	</div>
-	<div className="overlay-container">
-		<div className="overlay">
-			<div className="overlay-panel overlay-left">
-			<h2>Reverso Social</h2>
-			<h2>FEMsenior</h2>
-				<button className="ghost" onClick={handleSignInClick} id="signIn">Sign In</button>
-			</div>
-			<div className="overlay-panel overlay-right">
-				<p>Bienvenida a </p>
-				<h2>Reverso Social</h2>
-				<h2>FEMsenior</h2>
-			</div>
-		</div>
-	</div>
-</div>
-        </>
-    )
-}
-export default SignInUpForm
+  return (
+    <>
+      <div
+        className={`container ${
+          isRightPanelActive ? "right-panel-active" : ""
+        }`}
+        id="container"
+      >
+        <div className="form-container sign-up-container">
+          <form action="#">
+            <h2>Crea una cuenta</h2>
+
+            <input type="text" placeholder="Nombre" />
+            <input type="email" placeholder="Email" />
+            <input type="date" placeholder="Fecha de nacimiento" />
+            <input type="password" placeholder="Contraseña" />
+            <button>Entrar</button>
+          </form>
+        </div>
+        <div className="form-container sign-in-container">
+          <form action="#">
+            <h2>Accede a tu cuenta</h2>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Contraseña" />
+            <button>Entrar</button>
+            <p href="#">
+              No tienes cuenta? Accede{" "}
+              <span className="ghost" onClick={handleSignUpClick} id="signUp">
+                {" "}
+                Aqui
+              </span>
+            </p>
+          </form>
+        </div>
+
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <img src={logoReversoSocial}></img>
+              <img src={logoFemsenior}></img>
+              <button className="ghost" onClick={handleSignInClick} id="signIn">
+                Sign In
+              </button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h3>Bienvenida a </h3>
+			  <div>
+              <img src={logoReversoSocial}></img>
+              <img src={logoFemsenior}></img>
+			  </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default SignInUpForm;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import SignInUpForm from "../components/forms/logInForm/SignInUpForm";
 
 function Login() {
-  return <div>Login</div>;
+  return <div>Login
+    <SignInUpForm/>
+  </div>;
 }
 
 export default Login;
