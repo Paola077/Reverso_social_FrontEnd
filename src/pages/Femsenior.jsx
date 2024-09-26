44f0acc3795047d7016cd458b8b4c7c852927c84
CarolBV <carolina.botella.valero@gmail.com>
2024-09-26 12:12:53 +0200

    add manifestButton fem

--- src/pages/Femsenior.jsx
+++ src/pages/Femsenior.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Manifest from "../components/sections/manifest/Manifest";
+import ManifestButton from "../components/buttons/manifestButton/ManifestButton";
 
 function Femsenior() {
   return (
     <>
        <div className="manifestContainer">
       <Manifest/>
+      <ManifestButton/>
       </div>
       <Outlet />
     </>
