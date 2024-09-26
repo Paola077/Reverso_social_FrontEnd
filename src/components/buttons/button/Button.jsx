981f406b9f7a9be4f664b26f9d1865026cccd16d
Laura Pino <lpigueroa@gmail.com>
2024-09-26 12:46:29 +0200

    Manifest component and view more button in intro

--- src/components/buttons/button/Button.jsx
+++ src/components/buttons/button/Button.jsx
@@ -1,15 +1,30 @@
 import React from "react";
-import "./_Button.scss"
+import "./_Button.scss";
 
-export const Button = ({textButton, onClick, backgroundColor, border, color, type})=>{
-
-    return(
-        <button className="aceptCancel"
-        style={{backgroundColor: backgroundColor, border: border, color:color}}
-        onClick={onClick}
-        type={type}
-        >
-            {textButton}
-        </button>
-    )
-}
+export const Button = ({
+  textButton,
+  onClick,
+  backgroundColor,
+  border,
+  color,
+  type,
+  width,
+  height,
+}) => {
+  return (
+    <button
+      className="aceptCancel"
+      style={{
+        backgroundColor: backgroundColor,
+        border: border,
+        color: color,
+        width: width,
+        height: height,
+      }}
+      onClick={onClick}
+      type={type}
+    >
+      {textButton}
+    </button>
+  );
+};
