6b7ed618b6c6e7dc5ac2a4676fe4e42ec698eb26
indiakka <78439130+indiakka@user.noreply.github.com>
2024-10-02 14:18:43 +0200

    jwt decoder for getting user's data

--- src/components/forms/logInForm/SignInUpForm.jsx
+++ src/components/forms/logInForm/SignInUpForm.jsx
@@ -2,15 +2,14 @@ import React, { useState, useEffect } from "react";
 import "./_SignInUpForm.scss";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { userRegister, userLogin } from "../../../services/userApi";
-import logoReversoWhite from "../../../../public/images/RSLogoWhite.svg";
-import FSLogoWhite from "../../../../public/images/FSLogoWhite.svg";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { useAuth } from "../../../context/AuthContext";
+import { useAuth } from "../../../context/AuthContext"; // Asegúrate de que este sea el hook correcto
+import { jwtDecode } from "jwt-decode";
 
 const SignInUpForm = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const { login, isAuthenticated } = useAuth();
+  const { login, isAuthenticated } = useAuth(); // Usar el estado de autenticación global
   const [isRightPanelActive, setIsRightPanelActive] = useState(false);
   const queryClient = useQueryClient();
   const [data, setData] = useState(null);
@@ -25,11 +24,10 @@ const SignInUpForm = () => {
   });
 
   useEffect(() => {
-    const token = localStorage.getItem("authToken");
-    if (token) {
-      navigate("/reverso-social/femsenior");
+    if (isAuthenticated) {
+      navigate("/reverso-social/femsenior"); 
     }
-  }, [navigate]);
+  }, [isAuthenticated, navigate]);
 
   useEffect(() => {
     if (location.pathname === "/reverso-social/login") {
@@ -39,30 +37,12 @@ const SignInUpForm = () => {
     }
   }, [location.pathname]);
 
-  const resetForm = () => {
-    setForm({
-      name: "",
-      lastname: "",
-      email: "",
-      username: "",
-      password: "",
-      birthday: "",
-    });
-  };
-
   const handleChange = (e) => {
     const { name, value } = e.target;
     setForm({
       ...form,
       [name]: value,
     });
-  };
-
-  const handleRegister = async (e) => {
-    e.preventDefault();
-    setData(null);
-    setError(null);
-    registerMutation.mutate(form);
   };
 
   const handleLogin = async (e) => {
@@ -72,34 +52,38 @@ const SignInUpForm = () => {
     loginMutation.mutate(form);
   };
 
-  const registerMutation = useMutation({
-    mutationFn: (form) => userRegister(form),
-    onSuccess: (res) => {
-      setData(res);
-      resetForm();
-      setIsRightPanelActive(false);
-      queryClient.invalidateQueries({ queryKey: ["user"] });
-    },
-  });
 
   const loginMutation = useMutation({
     mutationFn: (form) => userLogin(form),
     onSuccess: (res) => {
-      setData(res);
-      localStorage.setItem("authToken", res.token); 
-      login(res.token); 
+      console.log("Respuesta completa del servidor:", res);
+
+      const accessToken = res?.accessToken;
+
+      if (accessToken) {
+        const decodedToken = jwtDecode(accessToken);
+        console.log("Token decodificado:", decodedToken);
+
+        const role = decodedToken?.authorities?.[0] || "USER"; 
+
+        localStorage.setItem("authToken", accessToken);
+        localStorage.setItem("user", JSON.stringify({ email: form.email }));
+        localStorage.setItem("role", role);
+
+        login(accessToken, { email: form.email }, role);
+        queryClient.invalidateQueries({ queryKey: ["user"] });
+
+        navigate("/reverso-social/femsenior");
+      } else {
+        console.error("No se recibió el token de acceso.");
+        setError("Error al recibir los datos de autenticación.");
+      }
     },
     onError: (res) => {
-      setError(res?.response.data);
+      console.error("Error al iniciar sesión:", res);
+      setError(res?.response?.data || "Error al iniciar sesión.");
     },
   });
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      navigate("/reverso-social/femsenior");
-    }
-  }, [isAuthenticated, navigate]);
-
   return (
     <div
       className={`signContainer ${
@@ -107,97 +91,6 @@ const SignInUpForm = () => {
       }`}
       id="container"
     >
-      <div
-        className="formContainer signUpContainer"
-        style={{ display: isRightPanelActive ? "block" : "none" }}
-      >
-        <form onSubmit={handleRegister}>
-          <h2 className="registerTitle">Crea una cuenta</h2>
-          <input
-            type="text"
-            placeholder="Nombre"
-            onChange={handleChange}
-            name="name"
-            value={form.name}
-            required
-          />
-          {data?.name && <p className="errorText">{data?.name.message}</p>}
-
-          <input
-            type="text"
-            placeholder="Apellido"
-            onChange={handleChange}
-            name="lastname"
-            value={form.lastname}
-            required
-          />
-          {data?.lastname && (
-            <p className="errorText">{data?.lastname.message}</p>
-          )}
-
-          <input
-            type="email"
-            placeholder="Email"
-            onChange={handleChange}
-            name="email"
-            value={form.email}
-            required
-          />
-          {data?.email && <p className="errorText">{data?.email.message}</p>}
-
-          <input
-            type="text"
-            placeholder="Nombre de usuario"
-            onChange={handleChange}
-            name="username"
-            value={form.username}
-            required
-          />
-          {data?.username && (
-            <p className="errorText">{data?.username.message}</p>
-          )}
-
-          <input
-            type="date"
-            placeholder="Fecha de nacimiento"
-            onChange={handleChange}
-            name="birthday"
-            value={form.birthday}
-            required
-          />
-          {data?.birthday && (
-            <p className="errorText">{data?.birthday.message}</p>
-          )}
-
-          <input
-            type="password"
-            placeholder="Contraseña"
-            onChange={handleChange}
-            name="password"
-            value={form.password}
-            required
-          />
-          {data?.password && (
-            <p className="errorText">{data?.password.message}</p>
-          )}
-
-          <button className="ghost" type="submit">
-            Registrar
-          </button>
-          <p>
-            Ya tienes cuenta? Accede{" "}
-            <Link
-              to="/reverso-social/login"
-              className="ghost"
-              onClick={() => setIsRightPanelActive(false)}
-              id="login"
-            >
-              Aquí
-            </Link>
-          </p>
-        </form>
-      </div>
-
       <div
         className="formContainer signInContainer"
         style={{ display: !isRightPanelActive ? "block" : "none" }}
@@ -225,7 +118,7 @@ const SignInUpForm = () => {
           {data?.password && (
             <p className="errorText">{data?.password.message}</p>
           )}
-          {error && <p className="errorText">{error?.message}</p>}
+          {error && <p className="errorText">{error}</p>}
 
           <button type="submit">Ingresar</button>
           <p>
@@ -247,15 +140,15 @@ const SignInUpForm = () => {
           <div className="overlayPanel overlayLeft">
             <h3>Bienvenida a </h3>
             <div className="overlayLogo">
-              <img src={logoReversoWhite} alt="Logo Reverso" />
-              <img src={FSLogoWhite} alt="Logo Femsenior" />
+              <img src="/images/RSLogoWhite.svg" alt="Logo Reverso" />
+              <img src="/images/FSLogoWhite.svg" alt="Logo Femsenior" />
             </div>
           </div>
           <div className="overlayPanel overlayRight">
             <h3>Bienvenida a </h3>
             <div className="overlayLogo">
-              <img src={logoReversoWhite} alt="Logo Reverso" />
-              <img src={FSLogoWhite} alt="Logo Femsenior" />
+              <img src="/images/RSLogoWhite.svg" alt="Logo Reverso" />
+              <img src="/images/FSLogoWhite.svg" alt="Logo Femsenior" />
             </div>
           </div>
         </div>
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -6,18 +6,25 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [role, setRole] = useState(null);
 
   useEffect(() => {
     const token = localStorage.getItem("authToken");
-    if (token) {
+    const user = localStorage.getItem("user");
+    const storedRole = localStorage.getItem("role");
+
+    if (token && user && storedRole) {
       setIsAuthenticated(true);
+      setRole(storedRole);
     }
     setLoading(false);
   }, []);
 
-  const login = (token, user) => {
+  const login = (token, user, role) => {
     localStorage.setItem("authToken", token);
     localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("role", role);
+
     setIsAuthenticated(true);
   };
 
