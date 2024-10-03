87f539545612ebbc51c92f1228a4b2965d446885
Deiximar Virriel <deiximar@gmail.com>
2024-10-03 16:30:53 +0200

    Create events and validate

--- src/components/modal/alerts/Alert.jsx
+++ src/components/modal/alerts/Alert.jsx
@@ -1,39 +1,20 @@
 import { Button } from "../../buttons/button/Button";
 import "./_Alert.scss";
 
+const Alert = ({ isOpen, onclose, alert, children }) => {
+  if (!isOpen) return null;
 
-const Alert = ({isOpen, onClose, alert}) => {
+  return (
+    <div className="overlay">
+      <div className="alertBox">
+        <button className="buttonExit" onClick={onclose}>
+          <img src="/icons/Exit.svg" alt="Cerrar formulario" />
+        </button>
+        <h3 className="alertText">{alert}</h3>
+        <div className="buttonsBox">{children}</div>
+      </div>
+    </div>
+  );
+};
 
-    if (!isOpen) return null; 
-
-    return(
-        <div className="overlay">
-            <div className="alertBox">
-                <button className="buttonExit" onClick={onClose}>
-                    <img src="/icons/Exit.svg" alt="Cerrar formulario" />
-                </button>
-                <h3 className="alertText">
-                    {alert} 
-                </h3>
-                <div className="buttonsBox">
-                    <Button 
-                    textButton={"Cancelar"}
-                    backgroundColor={"white"}
-                    width={"12.5rem"}
-                    height={"2.75rem"}
-                    border={"0.15rem solid #7176f8"}
-                    color={"#7176f8"}/>
-                    <Button 
-                    textButton={"Registrarse"}
-                    width={"12.5rem"}
-                    height={"2.75rem"}
-                    backgroundColor={"#7176f8"}
-                    border={"0.15rem solid #7176f8"}
-                    color={"white"}/>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default Alert
+export default Alert;
--- src/pages/FSForms.jsx
+++ src/pages/FSForms.jsx
@@ -3,98 +3,224 @@ import FSForm from "../components/forms/FSForms/FSForm";
 import { useParams } from "react-router-dom";
 
 const FSForms = () => {
-    const { formType } = useParams();
+  const { formType } = useParams();
 
-    const formConfigurations = {
-        evento: {
-            text: "NUEVO EVENTO",
-            fields: [
-                {title: "Título", type: "text", placeholder: "Título del evento", name: "title"},
-                {title: "Fecha", type: "date", placeholder: "", name: "date"},
-                {title: "Hora", type: "datetime", placeholder: "00:00", name: "time"},
-                {title: "Descripción", type: "textarea", placeholder: "Descripción del evento", name: "description"},
-                {title: "Max número de participantes", type: "number", min:"0",placeholder: "0", name: "maxParticipants"},
-                {title: "Modalidad", type: "select", name: "modality", options: [
-                    {label: "Presencial", value:"inPerson"},
-                    {label: "Online", value:"online"}
-                ]},
-                {title: "Sector", type: "select", name: "sector", optionText: "Selecciona el sector", options: [
-                    {label: "Administración y finanzas", value:"Administración y finanzas"},
-                    {label: "Atención al cliente y servicios", value:"Atención al cliente y servicios"},
-                    {label: "Atención y cuidados", value:"Atención y cuidados"},
-                    {label: "Artes y creatividades", value:"Artes y creatividades"},
-                    {label: "Dirección y ejecución", value:"Dirección y ejecución"},
-                    {label: "Hostelería y turismo", value:"Hostelería y turismo"},
-                    {label: "Psicología", value:"Psicología"},
-                    {label: "Sanidad", value:"Sanidad"},
-                    {label: "Servicios a la comunidad", value:"Servicios a la comunidad"},
-                    {label: "Tecnología", value:"Tecnología"},
-                    {label: "Terapias alternativas y desarrollo personal", value:"Terapias alternativas y desarrollo personal"}
-                ]},
-                {title: "Localización", type: "text", placeholder: "Dirección", name: "location"},
-            ]
+  const formConfigurations = {
+    evento: {
+      text: "NUEVO EVENTO",
+      fields: [
+        {
+          title: "Título",
+          type: "text",
+          placeholder: "Título del evento",
+          name: "title",
         },
-        mentoria: {
-            text:"NUEVA MENTORÍA",
-            fields: [
-                {title: "Título", type: "text", placeholder: "Título de la mentoría", name: "title"},
-                {title: "Sector", type: "select", name: "sector", optionText: "Selecciona el sector", options: [
-                    {label: "Administración y finanzas", value:"Administración y finanzas"},
-                    {label: "Atención al cliente y servicios", value:"Atención al cliente y servicios"},
-                    {label: "Atención y cuidados", value:"Atención y cuidados"},
-                    {label: "Artes y creatividades", value:"Artes y creatividades"},
-                    {label: "Dirección y ejecución", value:"Dirección y ejecución"},
-                    {label: "Hostelería y turismo", value:"Hostelería y turismo"},
-                    {label: "Psicología", value:"Psicología"},
-                    {label: "Sanidad", value:"Sanidad"},
-                    {label: "Servicios a la comunidad", value:"Servicios a la comunidad"},
-                    {label: "Tecnología", value:"Tecnología"},
-                    {label: "Terapias alternativas y desarrollo personal", value:"Terapias alternativas y desarrollo personal"}
-                ]},
-                {title: "Descripción", type: "textarea", placeholder: "Descripción de la mentoría", name: "description"},
-                {title: "Email", type: "email", placeholder: "Email", name: "email"},
-                {title: "Teléfono", type: "tel", placeholder: "Teléfono", name: "phone"},
-
-            ]
+        { title: "Fecha", type: "date", placeholder: "", name: "date" },
+        { title: "Hora", type: "datetime", placeholder: "00:00", name: "time" },
+        {
+          title: "Descripción",
+          type: "textarea",
+          placeholder: "Descripción del evento",
+          name: "description",
         },
-        curriculum: {
-            text:"SUBE TU CURRICULUM",
-            fields: [
-                {title: "Puesto", type: "text", placeholder: "Puesto con el que te ofreces", name: "work"},
-                {title: "Sector", type: "select", name: "sector", optionText: "Selecciona el sector", options: [
-                    {label: "Administración y finanzas", value:"Administración y finanzas"},
-                    {label: "Atención al cliente y servicios", value:"Atención al cliente y servicios"},
-                    {label: "Atención y cuidados", value:"Atención y cuidados"},
-                    {label: "Artes y creatividades", value:"Artes y creatividades"},
-                    {label: "Dirección y ejecución", value:"Dirección y ejecución"},
-                    {label: "Hostelería y turismo", value:"Hostelería y turismo"},
-                    {label: "Psicología", value:"Psicología"},
-                    {label: "Sanidad", value:"Sanidad"},
-                    {label: "Servicios a la comunidad", value:"Servicios a la comunidad"},
-                    {label: "Tecnología", value:"Tecnología"},
-                    {label: "Terapias alternativas y desarrollo personal", value:"Terapias alternativas y desarrollo personal"}
-                ]},
-                {title: "Descripción", type: "textarea", placeholder: "Describe tu experiencia", name: "description"},
-                {title: "Curriculum", type: "file", placeholder: "Selecciona", name: "curriculum", accept:"image/*,.pdf"},
-            ]
+        {
+          title: "Max número de participantes",
+          type: "number",
+          min: "0",
+          placeholder: "0",
+          name: "maxParticipants",
         },
-        recurso: {
-            text:"NUEVO RECURSO",
-            fields: [
-                {title: "Título", type: "text", placeholder: "Título del recurso", name: "resorce"},
-                {title: "Enlace", type: "url", placeholder: "Enlace del recurso", name: "link"},
-                {title: "Descripción", type: "textarea", placeholder: "Descripción", name: "description"},
-                {title: "Documento", type: "file", placeholder: "Selecciona un documento", name: "document", accept:"multimedia"},
-            ]
-        }
-    }
-    const currentForm = formConfigurations[formType];
-    return (
-        <FSForm 
-        text = {currentForm.text}
-        formFields = {currentForm.fields}
-        />
-    )
-}
+        {
+          title: "Modalidad",
+          type: "select",
+          name: "modality",
+          options: [
+            { label: "Presencial", value: "inPerson" },
+            { label: "Online", value: "online" },
+          ],
+        },
+        {
+          title: "Sector",
+          type: "select",
+          name: "sector",
+          optionText: "Selecciona el sector",
+          options: [
+            {
+              label: "Administración y Finanzas",
+              value: "Administración y Finanzas",
+            },
+            {
+              label: "Atención al Cliente y Servicios",
+              value: "Atención al Cliente y Servicios",
+            },
+            { label: "Atención y Cuidados", value: "Atención y Cuidados" },
+            { label: "Artes y Creatividades", value: "Artes y Creatividades" },
+            { label: "Dirección y Ejecución", value: "Dirección y Ejecución" },
+            { label: "Hostelería y Turismo", value: "Hostelería y Turismo" },
+            { label: "Psicología", value: "Psicología" },
+            { label: "Sanidad", value: "Sanidad" },
+            {
+              label: "Servicios a la Comunidad",
+              value: "Servicios a la Comunidad",
+            },
+            { label: "Tecnología", value: "Tecnología" },
+            {
+              label: "Terapias Alternativas y Desarrollo Personal",
+              value: "Terapias Alternativas y Desarrollo Personal",
+            },
+          ],
+        },
+        {
+          title: "Localización",
+          type: "text",
+          placeholder: "Dirección",
+          name: "location",
+        },
+      ],
+    },
+    mentoria: {
+      text: "NUEVA MENTORÍA",
+      fields: [
+        {
+          title: "Título",
+          type: "text",
+          placeholder: "Título de la mentoría",
+          name: "title",
+        },
+        {
+          title: "Sector",
+          type: "select",
+          name: "sector",
+          optionText: "Selecciona el sector",
+          options: [
+            {
+              label: "Administración y Finanzas",
+              value: "Administración y Finanzas",
+            },
+            {
+              label: "Atención al Cliente y Servicios",
+              value: "Atención al Cliente y Servicios",
+            },
+            { label: "Atención y Cuidados", value: "Atención y Cuidados" },
+            { label: "Artes y Creatividades", value: "Artes y Creatividades" },
+            { label: "Dirección y Ejecución", value: "Dirección y Ejecución" },
+            { label: "Hostelería y Turismo", value: "Hostelería y Turismo" },
+            { label: "Psicología", value: "Psicología" },
+            { label: "Sanidad", value: "Sanidad" },
+            {
+              label: "Servicios a la Comunidad",
+              value: "Servicios a la Comunidad",
+            },
+            { label: "Tecnología", value: "Tecnología" },
+            {
+              label: "Terapias Alternativas y Desarrollo Personal",
+              value: "Terapias Alternativas y Desarrollo Personal",
+            },
+          ],
+        },
+        {
+          title: "Descripción",
+          type: "textarea",
+          placeholder: "Descripción de la mentoría",
+          name: "description",
+        },
+        { title: "Email", type: "email", placeholder: "Email", name: "email" },
+        {
+          title: "Teléfono",
+          type: "tel",
+          placeholder: "Teléfono",
+          name: "phone",
+        },
+      ],
+    },
+    curriculum: {
+      text: "SUBE TU CURRICULUM",
+      fields: [
+        {
+          title: "Puesto",
+          type: "text",
+          placeholder: "Puesto con el que te ofreces",
+          name: "work",
+        },
+        {
+          title: "Sector",
+          type: "select",
+          name: "sector",
+          optionText: "Selecciona el sector",
+          options: [
+            {
+              label: "Administración y Finanzas",
+              value: "Administración y Finanzas",
+            },
+            {
+              label: "Atención al Cliente y Servicios",
+              value: "Atención al Cliente y Servicios",
+            },
+            { label: "Atención y Cuidados", value: "Atención y Cuidados" },
+            { label: "Artes y Creatividades", value: "Artes y Creatividades" },
+            { label: "Dirección y Ejecución", value: "Dirección y Ejecución" },
+            { label: "Hostelería y Turismo", value: "Hostelería y Turismo" },
+            { label: "Psicología", value: "Psicología" },
+            { label: "Sanidad", value: "Sanidad" },
+            {
+              label: "Servicios a la Comunidad",
+              value: "Servicios a la Comunidad",
+            },
+            { label: "Tecnología", value: "Tecnología" },
+            {
+              label: "Terapias Alternativas y Desarrollo Personal",
+              value: "Terapias Alternativas y Desarrollo Personal",
+            },
+          ],
+        },
+        {
+          title: "Descripción",
+          type: "textarea",
+          placeholder: "Describe tu experiencia",
+          name: "description",
+        },
+        {
+          title: "Curriculum",
+          type: "file",
+          placeholder: "Selecciona",
+          name: "curriculum",
+          accept: "image/*,.pdf",
+        },
+      ],
+    },
+    recurso: {
+      text: "NUEVO RECURSO",
+      fields: [
+        {
+          title: "Título",
+          type: "text",
+          placeholder: "Título del recurso",
+          name: "resorce",
+        },
+        {
+          title: "Enlace",
+          type: "url",
+          placeholder: "Enlace del recurso",
+          name: "link",
+        },
+        {
+          title: "Descripción",
+          type: "textarea",
+          placeholder: "Descripción",
+          name: "description",
+        },
+        {
+          title: "Documento",
+          type: "file",
+          placeholder: "Selecciona un documento",
+          name: "document",
+          accept: "multimedia",
+        },
+      ],
+    },
+  };
+  const currentForm = formConfigurations[formType];
+  return <FSForm text={currentForm.text} formFields={currentForm.fields} />;
+};
 
 export default FSForms;
