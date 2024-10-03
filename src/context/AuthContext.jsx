660e16bd302eda6316b2984fa4c01aa2a4e4a96c
MabelZar <mabel.zar.esp@gmail.com>
2024-10-03 12:53:53 +0200

    show DynamicTab  component by user Femsenior

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import React from "react";
 
-const AuthContext = createContext();
+export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -26,6 +26,7 @@ export const AuthProvider = ({ children }) => {
     localStorage.setItem("role", role);
 
     setIsAuthenticated(true);
+    setRole(role);
   };
 
   const logout = () => {
@@ -35,7 +36,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, role, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
