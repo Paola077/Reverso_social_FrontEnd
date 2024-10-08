5fe8a95cfa8c8c6d65cb30be490fe9353fb158ec
Laura Pino <lpigueroa@gmail.com>
2024-10-08 09:38:06 +0200

    EventsApi implemented

---
+++ src/services/employApi.jsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { EMPLOY_URL, EMPLOY_URL_ID } from "../config/url";
+
+export const createEmployOffer = async (data, token) => {
+  let headers = undefined;
+  if (token) {
+    headers = { Authorization: `Bearer ${token}` };
+  }
+  const res = await axios.post(EMPLOY_URL, data, { headers });
+  return res.data;
+};
+
+export const getEmployById = async (id) => {
+  const res = await axios.get(EMPLOY_URL_ID(id));
+  return res.data;
+};
+
+export const getAllEmploys = async () => {
+  const res = await axios.get(EMPLOY_URL);
+  return res.data;
+};
+
+export const updateEmployOffer = async (id, data, token) => {
+  let headers = undefined;
+  if (token) {
+    headers = { Authorization: `Bearer ${token}` };
+  }
+  const res = await axios.put(EMPLOY_URL_ID(id), data, { headers });
+  return res.data;
+};
+
+export const deleteEmployOffer = async (id, token) => {
+  let headers = undefined;
+  if (token) {
+    headers = { Authorization: `Bearer ${token}` };
+  }
+  const res = await axios.delete(EMPLOY_URL_ID(id), { headers });
+  return res.data;
+};
