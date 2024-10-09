03f2587c4bff44f90efa094d7e5cd576e7c3caea
Cris <crispr.zde@gmail.com>
2024-10-09 20:48:48 +0200

    Added responsive to Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,6 +7,9 @@ import { RequestButton } from "../buttons/requestButton/RequestsButton";
 
 
 export const Footer = () =>{
+    const buttonWidth = window.innerWidth < 420 ? "10rem" : "19rem"; 
+    const buttonHeight = window.innerWidth < 420 ? "1.7rem" : "2.2rem"; 
+    const buttonFontSize = window.innerWidth < 420 ? "0.6rem" : "0.9rem";
 
     const navigate = useNavigate();
     
@@ -30,10 +33,10 @@ return(
             <div className="footerSocials">
                 <RequestButton 
                     text={"FORMULARIO DE PETICIONES"} 
-                    width={"16rem"} 
-                    height={"2rem"}
+                    width={buttonWidth} 
+                    height={buttonHeight}
                     marginRight={"1.5rem"} 
-                    fontSize={"0.8rem"}
+                    fontSize={buttonFontSize}
                     color={"#090d6d"}
                     onClick={()=> navigate("formulario/peticiones")}/>
                 <BsFacebook className="footerFb" onClick={handleClick2}/>
